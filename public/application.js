c5959d01584af520ac6517c31e1389679c678a4e
triple-eh <aleckaleksandrov@gmail.com>
2016-06-29 17:44:58 -0700

    Main slideshow functionality is implemented

--- public/application.js
+++ public/application.js
@@ -2,14 +2,14 @@ $(function() {
 
   var page = 1;
   var flickrResponse;
-  var pages;
+  var photoPages;
   
   var img = $("<img />");
   img.css('display','none');
   img.appendTo("body");
 
-  var getFlickrImages =
-    $.ajax({
+ var getFlickrData = 
+   $.ajax({
       url: 'https://api.flickr.com/services/rest/',
       data: {
         'method': 'flickr.photos.search',
@@ -20,33 +20,24 @@ $(function() {
       },
       dataType:'jsonp',
       jsonpCallback:'jsonFlickrApi'
-      // success: function(data) {
-      //   flickrResponse = data;
-      // }
     });
 
+ getFlickrData.then(function (data) {
+  flickrPhotoArray = data.photos.photo;
+  loopThroughImages(flickrPhotoArray);
+ });
+ 
   var images = new Array();
   var time = 1000;
   var delayStay = 5000;
   var delaySwitch = 1000;
 
-  // getFlickrImages.then(function(data) {
-  //   console.log(data);
-  // });
 
-  getFlickrImages.then(function(data) {
-    $.each(data.photos.photo, function(index, flickrPhoto) {
-      // var imgSrc = "https://farm"+ obj.farm + ".staticflickr.com/" + obj.server + "/" + obj.id + "_" + obj.secret + ".jpg";
+  function loopThroughImages(flickrPhotoArray) {
+    $.each(flickrPhotoArray, function(index, flickrPhoto) {
       showImage(img,getImageSrc(flickrPhoto));
     });
-  });
-
-  // getFlickrImages.then(function(data) {
-  //   $.each(data.photos.photo, function(index, obj) {
-  //     var imgSrc = "https://farm"+ obj.farm + ".staticflickr.com/" + obj.server + "/" + obj.id + "_" + obj.secret + ".jpg";
-  //     showImage(img,imgSrc);
-  //   });
-  // });
+  };
 
   function showImage(el, src) {
     setTimeout( function() { 
@@ -68,3 +59,5 @@ $(function() {
   };
 
 });
+
+
